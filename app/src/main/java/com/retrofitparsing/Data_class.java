f36f70df64f6628f9c1c9f0bf98d314bf3774258
sysadminamit <amitc8250@gmail.com>
2018-10-11 22:42:50 +0530

    Change some code and push it on github

--- app/src/main/java/com/retrofitparsing/Data_class.java
+++ app/src/main/java/com/retrofitparsing/Data_class.java
@@ -1,18 +1,18 @@
 package com.retrofitparsing;
 
-import static android.R.attr.id;
+
 
 /**
  * Created by Nilagnik on 24-09-2017.
  */
-
+//updated by amitc8250
 public class Data_class {
 
     private String title = "";
     private String description = "";
     private String time="";
 
-    public Data_class(String title,String description,String time) {
+    Data_class(String title, String description, String time) {
         this.title=title;
         this.description=description;
         this.time=time;
